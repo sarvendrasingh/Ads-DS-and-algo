e9683c42933103e32344cb782c32c75504820db7
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-05-22 12:54:59 +0530

    power using recursion

--- Recursion/power.java
+++ Recursion/power.java
@@ -1,4 +1,27 @@
 package Recursion;
 
+import java.util.Scanner;
+
+// n to the power of m (using recursion)
 public class power {
+    static int power(int m, int n){
+        if(n==0){return 1;}
+        //  return m*power(m,n-1);
+        if(n%2 ==0){
+            return power(m*m,n/2);
+        }
+        else{
+            return m*power(m*m,(n-1)/2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m,n;
+        m = sc.nextInt();
+        n = sc.nextInt();
+        int ans;
+        ans = power(m,n);
+        System.out.println(n+" to the power of "+m+" is "+ans);
+    }
 }
